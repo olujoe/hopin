30c602eb55c0d1c11f1606966467a6a1b5468b4e
Olugbenga Ogunbusola <olugbenga.ogunbusola@admins-MacBook-Pro.local>
2021-04-26 22:35:05 +0100

    updating api test

--- api-tests/welcome-api-post-request.js
+++ api-tests/welcome-api-post-request.js
@@ -12,37 +12,65 @@ const tempFile      = './tempId.json';
 
 describe('Given a request is sent to the correct endpoint', function() {
 
-  it('Then the api should return a 200 response code and JSON object', (done) => {
-    api.post('/')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end(function (err, res) {
-        if (err) return done(err);
-        done();
+    it('Then the api should return a 200 response code and JSON object', (done) => {
+        api.post('/')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function (err, res) {
+            if (err) return done(err);
+            done();
+        });
     });
-  });
 
-  it('Then the api should return a 200 response code with expected userId details', (done) => {
-    api.post('/')
-        .expect(200)
-        .expect('Content-Type', /json/)
+    it('Then the api should return a 200 response code with expected userId details', (done) => {
+        api.post('/')
+            .end(function (err, res) {
+            if (err) return done(err);
+            // console.log(res.body.timestamp);
+            // console.log(res.body.customers[0].employees);
+            var date = Date().split(" "),
+            newDate  = (date[0]+" "+date[1]+" "+date[2]+" "+date[3]);
+            expect(res.body.timestamp).to.equal(newDate);
+            expect(res.body.customers[0]).to.have.property("id",1);
+            expect(res.body.customers[0]).to.have.property("name","Americas Inc.");
+            expect(res.body.customers[0]).to.have.property("employees",10);
+            expect(res.body.customers[0].contactInfo).to.have.property("name","John Smith");
+            expect(res.body.customers[0].contactInfo).to.have.property("email", "jsmith@americasinc.com");
+            expect(res.body.customers[0]).to.have.property("size","Small");
+            done()
+        });
+    });
+
+    it('Then the api should return correct size per employee numbers', (done) => {
+        api.post('/')
         .end(function (err, res) {
         if (err) return done(err);
-        console.log(res.body.timestamp);
-        // console.log(res.body.customers[0]);
-        var date = Date().split(" "),
-        newDate  = (date[0]+" "+date[1]+" "+date[2]+" "+date[3]);
-        // expect(res.body.timestamp).to.equal("Mon Apr 26 2021");
-        expect(res.body.timestamp).to.equal(newDate);
-        expect(res.body.customers[0]).to.have.property("id",1);
-        expect(res.body.customers[0]).to.have.property("name","Americas Inc.");
-        expect(res.body.customers[0]).to.have.property("employees",10);
-        expect(res.body.customers[0].contactInfo).to.have.property("name","John Smith");
-        expect(res.body.customers[0].contactInfo).to.have.property("email", "jsmith@americasinc.com");
-        expect(res.body.customers[0]).to.have.property("size","Small");
-        done()
+        // var i;
+            for (var i = 0; i < 6; i++) {
+                if (res.body.customers[i].employees <= 10)
+                    try {
+                        expect(res.body.customers[i]).to.have.property("size","Small");
+                        throw err
+                    } catch (err) {
+                        console.log(err)
+                    }
+                else if (res.body.customers[i].employees <= 1000)
+                    try {
+                        expect(res.body.customers[i]).to.have.property("size","Medium");
+                        throw err
+                    } catch (err) {
+                        console.log(err)
+                    }
+                else  (res.body.customers[i].employees > 1000)
+                    try {
+                        expect(res.body.customers[i]).to.have.property("size","Big");
+                        throw err
+                    } catch (err) {
+                        console.log(err)
+                    }
+            }
+            done()
+        });
     });
-  });
-  
 
 });
